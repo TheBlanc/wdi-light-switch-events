e78738834cca0c9ac4c2e599d68ae3bab1324189
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-07-16 16:08:36 -0400

    Made the light switch

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
+var light = document.querySelector('.light')
 
+var plate = document.querySelector('.plate');
 
+var button = plate.firstElementChild;
+
+var headingStatus = document.querySelector('h1.status');
+
+button.addEventListener('click', function() {
+  this.classList.toggle('on');
+  this.classList.toggle('off');
+
+  light.classList.toggle('light');
+  light.classList.toggle('dark');
+
+  if (light.classList.contains('dark')) {
+    headingStatus.innerHTML = "Who turned out the lights?!";
+  } else {
+    headingStatus.innerHTML = "It's so bright in here!";
+  }
+});
